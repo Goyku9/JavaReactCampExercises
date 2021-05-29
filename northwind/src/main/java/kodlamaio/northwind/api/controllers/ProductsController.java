89e7145a78cf8aa5783dc2bd4c215feb8acf069e
oyku <49123232+Goyku9@users.noreply.github.com>
2021-05-29 19:47:20 +0300

    swagger added

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @RestController
@@ -17,16 +21,20 @@ public class ProductsController {
 
 	private ProductService productService;
 	
-	@Autowired
+	@Autowired //ProductService interface'ini implement eden somut sınıfı bulup newliyor. Yani productManager'ı.
 	public ProductsController(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
 	@GetMapping("/getall") //get request olduğu için getmapping kullandım.
-	public List<Product> getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
 		
+	}
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);
 	}
 
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,17 +3,20 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
 
 
+@Data
 @Entity //product entitydir.//veritabanı nesnesi.
 @Table(name="products") //veritabınında hangi tabloya karşılık geldiğini belirtir.
 public class Product {
 	
 	@Id //sorgular bu id'ye göre yapılandırılır.
-	@GeneratedValue  //id'nin tek tek arttırıldığı. Otomatik
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //id'nin tek tek arttırıldığı. Otomatik
 	@Column(name="product_id")
 	private int id;
 	
